608ebad984cabc8da954b81f9f1c7a586fff8dd8
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-04-13 21:02:06 +0200

    Sums all multiples of 3 or 5 in a given number
    Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.

---
+++ multiples_of_3_and_5.js
@@ -0,0 +1,25 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+*/
+
+
+
+function multiplesOf3and5(number) {
+  let sum = 0;
+  for (let i=1; i<number; i++){
+    if ( i % 3===0 || i % 5===0 ){
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+// TESTS: 
+console.log(multiplesOf3and5(10));    // 23
+console.log(multiplesOf3and5(49));    // 543
+console.log(multiplesOf3and5(100));   // 2318
+console.log(multiplesOf3and5(1000));  // 233168
+console.log(multiplesOf3and5(8456));  // 16687353
+console.log(multiplesOf3and5(19564)); // 89301183 
